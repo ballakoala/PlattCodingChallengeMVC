d4a76ab39c8899d18e7038f885239e5a0cd0e900
BallaKoala <no.jeremy1212@gmail.com>
2022-05-02 00:46:35 -0700

    up till now i didnt realize i could use newtonsoft, and that base classes should have different property names. started around 11pm yesterday

--- PlattCodingChallenge/Controllers/HomeController.cs
+++ PlattCodingChallenge/Controllers/HomeController.cs
@@ -1,5 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PlattCodingChallenge.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
 
 namespace PlattCodingChallenge.Controllers
 {
@@ -42,9 +48,89 @@ namespace PlattCodingChallenge.Controllers
 			var model = new VehicleSummaryViewModel();
 
 			// TODO: Implement this controller action
-			// YEAHH BOI GONNA DO STUFF HERERERERE!!!
+
+			SWAPIVehicleResults vehicleResults = GetSWAPIVehicles();
+
+			vehicleResults.results.Where(v => v.cost_in_credits != "unknown");
+
+			model.VehicleCount = vehicleResults.count;
 
 			return View(model);
+		}
+
+		public class SWAPIVehicleResults
+        {
+			public int count { get; set; }
+            public string next { get; set; }
+            public string previous { get; set; }
+			public List<SWAPVehicle> results { get; set; }
+		}
+
+		public class SWAPVehicle
+        {
+            public string cargo_capacity { get; set; }
+            public string consumables { get; set; }
+            public string cost_in_credits { get; set; }
+            public string created { get; set; }
+            public string crew { get; set; }
+            public string edited { get; set; }
+            public string length { get; set; }
+            public string manufacturer { get; set; }
+            public string max_atmosphering_speed { get; set; }
+            public string model { get; set; }
+            public string name { get; set; }
+            public string passengers { get; set; }
+            //public string pilots { get; set; }
+            public List<string> films { get; set; }
+            public string url { get; set; }
+            public string vehicle_class { get; set; }
+        }
+
+		// https://stackoverflow.com/questions/9620278/how-do-i-make-calls-to-a-rest-api-using-c
+		private SWAPIVehicleResults GetSWAPIVehicles(SWAPIVehicleResults previousVehicleResults = null)
+		{
+			if (previousVehicleResults == null)
+            {
+				previousVehicleResults = new SWAPIVehicleResults();
+
+			}
+			HttpClient httpClient = new HttpClient();
+			Uri baseUri = string.IsNullOrWhiteSpace(previousVehicleResults?.next) 
+				? new Uri(previousVehicleResults.next) 
+				: new Uri("https://swapi.dev/api/vehicles/");
+			httpClient.BaseAddress = baseUri;
+
+			httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+			HttpResponseMessage response = httpClient.GetAsync(baseUri).Result;
+			if (response.IsSuccessStatusCode)
+			{
+				var dataObjects = response.Content.ReadAsStringAsync().Result;
+
+				if (previousVehicleResults != null)
+				{
+					SWAPIVehicleResults vehicleSummaryViewModel = JsonSerializer.Deserialize<SWAPIVehicleResults>(dataObjects);
+					vehicleSummaryViewModel.results.AddRange(previousVehicleResults.results);
+					previousVehicleResults = vehicleSummaryViewModel;
+				}
+				else
+                {
+					previousVehicleResults = JsonSerializer.Deserialize<SWAPIVehicleResults>(dataObjects);
+				}
+
+				if (previousVehicleResults.next != null)
+                {
+					previousVehicleResults = GetSWAPIVehicles(previousVehicleResults);
+                }
+			}
+			else
+			{
+				//failed request
+			}
+
+			httpClient.Dispose();
+
+			return previousVehicleResults;
 		}
     }
 }
